d5105ab79c0694c99953421bbce4f8fb6998e908
methias <tmn5@pm.me>
2020-02-01 16:00:01 +0200

    add draft of Story component

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { getStoryIds } from '../services/hnApi';
+import { getStoryIds, getStory } from '../services/hnApi';
+import { Story } from '../components/Story';
 
 export const StoriesContainer = () => {
 	const [storyIds, setStoryIds] = useState([]);
@@ -8,5 +9,5 @@ export const StoriesContainer = () => {
 		getStoryIds().then(data => setStoryIds(data));
 	}, []);
 
-	return <p>{JSON.stringify(storyIds)}</p>;
+	return storyIds.map(storyId => <Story key={storyId} storyId={storyId} />);
 };
