314df90364d884de8d8a47f6ae971696e4b9a445
methias <tmn5@pm.me>
2020-02-01 15:34:01 +0200

    refactor stories fetch to StoriesContainer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { getStoryIds } from './services/hnApi';
+import React from 'react';
+import { StoriesContainer } from './containers/StoriesContainer';
 
-export const App = () => {
-	const [storyIds, setStoryIds] = useState([]);
-
-	useEffect(() => {
-		getStoryIds().then(data => setStoryIds(data));
-	}, []);
-
-	return (
-		<p>{JSON.stringify(storyIds)}</p>
-	)
-}
+export const App = () => <StoriesContainer />;
 
