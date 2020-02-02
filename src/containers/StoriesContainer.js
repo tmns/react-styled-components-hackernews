ea04bc3e09eec13b6596f6a0336e4bf724b4f9cf
methias <tmn5@pm.me>
2020-02-02 12:36:17 +0200

    implement draft of infinite scroll functionality

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import { getStoryIds, getStory } from '../services/hnApi';
 import { Story } from '../components/Story';
 import { GlobalStyle, StoriesContainerWrapper } from '../styles/StoriesContainerStyles';
+import { useInfiniteScroll } from '../hooks/useInfiniteScroll';
 
 export const StoriesContainer = () => {
 	const [storyIds, setStoryIds] = useState([]);
+	const { count } = useInfiniteScroll();
 
 	useEffect(() => {
 		getStoryIds().then(data => setStoryIds(data));
@@ -15,7 +17,7 @@ export const StoriesContainer = () => {
 			<GlobalStyle />
 			<StoriesContainerWrapper data-test-id="stories-container">
 				<h1>Hacker News Stories</h1>
-				{storyIds.map(storyId => (
+				{storyIds.slice(0, count).map(storyId => (
 					<Story key={storyId} storyId={storyId} />
 				))}
 			</StoriesContainerWrapper>
---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { MAX_STORIES, STORY_INCREMENT } from '../constants';
+
+export const useInfiniteScroll = () => {
+	const [loading, setLoading] = useState(false);
+	const [count, setCount] = useState(STORY_INCREMENT);
+
+	const handleScroll = () => {
+		if (
+			window.innerHeight + document.documentElement.scrollTop !== 
+				document.documentElement.offsetHeight || 
+			loading
+		) {
+			return false;
+		}
+
+		setLoading(true);
+	};
+
+	useEffect(() => {
+		if (!loading) return;
+
+		if (count + STORY_INCREMENT >= MAX_STORIES) {
+			setCount(MAX_STORIES);
+		} else {
+			setCount(count + STORY_INCREMENT);
+		}
+
+		setLoading(false);
+	}, [loading]);
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, []);
+
+	return { count };
+};
