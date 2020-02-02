b1060185286f7fc86145ac9a27440955a2bf8b95
methias <tmn5@pm.me>
2020-02-02 13:07:10 +0200

    add test of App component

---
+++ src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+	by: 'Test Author',
+	id: 1,
+	time: 1567209822,
+	title: 'Test Title',
+	url: 'https://test.com/test',
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+	by: undefined,
+	id: undefined,
+	time: undefined,
+	title: undefined,
+	url: undefined, 
+}
